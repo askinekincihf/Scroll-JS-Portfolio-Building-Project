287815918ce50ec1ce759b954f2e0f9a2cfd2693
askinekinci <askinekincihf@gmail.com>
2021-01-06 19:50:11 +0000

    added navToogle and window scroll features.

--- app.js
+++ app.js
@@ -4,10 +4,47 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector('.links');
+
+navToggle.addEventListener("click", function () {
+    // linksContainer.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    }
+    else {
+        linksContainer.style.height = 0;
+    }
+});
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
 // ********** fixed navbar ************
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
 
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    }
+    else {
+        navbar.classList.remove("fixed-nav")
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add("show-link")
+    }
+    else {
+        topLink.classList.remove("show-link")
+    }
+})
 // ********** smooth scroll ************
 // select links
